00588fc1c4bbb28252d6ffd38abf4591e54fef39
JJ <jj.orozcolopez.13@gmail.com>
2019-07-11 13:14:15 -0600

    Adding resource files and some languages
    Adding english, spanish and italian

---
+++ ASP-Net-MVC-MultiLanguage-Example/LanguageMang.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using ASP_Net_MVC_MultiLanguage_Example.Models;
+
+namespace ASP_Net_MVC_MultiLanguage_Example
+{
+    public class LanguageMang
+    {
+        public static List<Language> AvailableLanguage = new List<Language> {
+            new Language {
+                LanguageFullName = "English", LanguageCultureName = "en"
+            },
+            new Language {
+                LanguageFullName = "Español", LanguageCultureName = "es"
+            },
+            new Language {
+                LanguageFullName = "Italiano", LanguageCultureName = "it"
+            },
+        };
+        public static bool IsLanguageAvailable(string lang)
+        {
+            return AvailableLanguage.Where(a => a.LanguageCultureName.Equals(lang)).FirstOrDefault() != null ? true : false;
+        }
+        public static string GetDefaultLanguage()
+        {
+            return AvailableLanguage[0].LanguageCultureName;
+        }
+        public void SetLanguage(string lang)
+        {
+            try
+            {
+                if (!IsLanguageAvailable(lang)) lang = GetDefaultLanguage();
+                var cultureInfo = new CultureInfo(lang);
+                Thread.CurrentThread.CurrentUICulture = cultureInfo;
+                Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(cultureInfo.Name);
+                HttpCookie langCookie = new HttpCookie("culture", lang);
+                langCookie.Expires = DateTime.Now.AddYears(1);
+                HttpContext.Current.Response.Cookies.Add(langCookie);
+            }
+            catch (Exception) { }
+        }
+    }
+}
---
+++ ASP-Net-MVC-MultiLanguage-Example/Models/Language.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASP_Net_MVC_MultiLanguage_Example.Models
+{
+    public class Language
+    {
+        public string LanguageFullName
+        {
+            get;
+            set;
+        }
+        public string LanguageCultureName
+        {
+            get;
+            set;
+        }
+    }
+}
