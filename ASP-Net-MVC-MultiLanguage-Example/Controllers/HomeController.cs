601d15cc5a83df53bba7a4d28011723682d40633
JJ <jj.orozcolopez.13@gmail.com>
2019-07-11 13:27:52 -0600

    Adding language controllers and management
    Implementing management of languages, models and controllers.

---
+++ ASP-Net-MVC-MultiLanguage-Example/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using ASP_Net_MVC_MultiLanguage_Example.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP_Net_MVC_MultiLanguage_Example.Controllers
+{
+    public class HomeController : MyController
+    {
+        // GET: Home    
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Registration r)
+        {
+            return View(r);
+        }
+        public ActionResult ChangeLanguage(string lang)
+        {
+            new LanguageMang().SetLanguage(lang);
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
